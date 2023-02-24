923e24fc3cd7b8f18122b3f5bfcab32b0defd258
Ziru Zou <jiyucoding@yahoo.com>
2023-02-24 17:46:12 -0500

    complete lab

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,14 @@
 import re
 
-my_pattern = r""
+
+my_pattern = r"[A-Z].*today.*"
 my_regex = re.compile(my_pattern)
 
+
+
+# [A-Z] => first letter is one of A-Z
+# . => any character (except for line terminators)
+# * => the previous token between zero and unlimited times, as many times as possible, giving back as needed (greedy)
+# today => the characters today literally (case sensitive)
+# . => any character (except for line terminators)
+# * => the previous token between zero and unlimited times, as many times as possible, giving back as needed (greedy)
